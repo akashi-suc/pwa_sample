ae567a8f2fc83a706e0a2402f4ce95a5b449a95b
Akashi <ohdan@success-corp.co.jp>
2019-09-20 10:28:30 +0900

    service-workerのソース変更

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,11 @@
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function(event) {
   console.log('[ServiceWorker] Install');
+  event.waitUntil(self.skipWaiting());
 });
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', function(event) {
   console.log('[ServiceWorker] Activate');
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', function(event) {
